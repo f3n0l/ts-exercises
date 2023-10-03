8f83823005066aacce57fb122b9e1a87c518582b
Tolga Simsek <tol_simsek@live.de>
2023-10-03 15:12:59 +0200

    tree and search

--- fourth-ex/src/binarySearchTree.ts
+++ fourth-ex/src/binarySearchTree.ts
@@ -2,3 +2,63 @@
 // search for specific value in bst, return "true" or "false" if exists
 // delete value
 // implement in-order, pre-order, post order methods
+
+
+class TreeNode {
+    value: number;
+    left: TreeNode | null = null;
+    right: TreeNode | null = null;
+
+    constructor(value: number) {
+        this.value = value;
+    }
+}
+
+export class BinarySearchTree {
+    root: TreeNode | null = null;
+
+    insert(value: number): void {
+        const newNode = new TreeNode(value);
+        if (!this.root) {
+            this.root = newNode;
+        } else {
+            this.insertNode(this.root, newNode);
+        }
+    }
+
+    private insertNode(node: TreeNode, newNode: TreeNode): void {
+        if (newNode.value < node.value) {
+            if (node.left === null) {
+                node.left = newNode;
+            } else {
+                this.insertNode(node.left, newNode);
+            }
+        } else {
+            if (node.right === null) {
+                node.right = newNode;
+            } else {
+                this.insertNode(node.right, newNode);
+            }
+        }
+    }
+
+    search(value: number): boolean {
+        return this.searchNode(this.root, value);
+    }
+
+    private searchNode(node: TreeNode | null, value: number): boolean {
+        if (node === null) {
+            return false;
+        }
+
+        if (value < node.value) {
+            return this.searchNode(node.left, value);
+        } else if (value > node.value) {
+            return this.searchNode(node.right, value);
+        } else {
+            return true;
+        }
+    }
+
+   
+}
