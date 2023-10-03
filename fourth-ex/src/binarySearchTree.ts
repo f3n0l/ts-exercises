3d92570cb754db51075759723862cc8953e99fe2
Tolga Simsek <tol_simsek@live.de>
2023-10-03 15:14:55 +0200

    cleanup

--- fourth-ex/src/binarySearchTree.ts
+++ fourth-ex/src/binarySearchTree.ts
@@ -1,9 +1,3 @@
-// add new Value to BST
-// search for specific value in bst, return "true" or "false" if exists
-// delete value
-// implement in-order, pre-order, post order methods
-
-
 class TreeNode {
     value: number;
     left: TreeNode | null = null;
